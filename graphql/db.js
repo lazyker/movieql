75c1c42adfac4ae2424ce20b514d3908e4c3f2a6
lazyker <lazyker@naver.com>
2019-10-06 23:21:59 +0900

    movie to change

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,48 @@
-export const people = [
-    {
-        id: 0,
-        name: "lazyker",
-        age: 37,
-        gender: "female"
-    },
-    {
-        id: 1,
-        name: "two",
-        age: 22,
-        gender: "male"
-    },
-    {
-        id: 2,
-        name: "three",
-        age: 37,
-        gender: "female"
-    },
+export let movies = [
+  {
+    id: 0,
+    name: "lazyker",
+    age: 37,
+    gender: "female"
+  },
+  {
+    id: 1,
+    name: "two",
+    age: 22,
+    gender: "male"
+  },
+  {
+    id: 2,
+    name: "three",
+    age: 37,
+    gender: "female"
+  },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id)
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id)
+  return filteredMovies[0];
+}
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  }
+
+  movies.push(newMovie);
+  return newMovie;
+}
+
+export const deleteMovie = id => {
+  const cleanMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanMovies.length) {
+    movies = cleanMovies;
+    return true;
+  } else {
+    return false;
+  }
 }
